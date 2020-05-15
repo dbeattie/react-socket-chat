6f47c47620fa14bc252f57dc55414b08a68de187
Darren Beattie <darren@fettle.ca>
2020-05-15 15:03:50 -0700

    add topics colum & chat bubbles

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -3,6 +3,12 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+//import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import Chip from '@material-ui/core/Chip';
 
 const styles = theme => ({
   root: {
@@ -10,6 +16,26 @@ const styles = theme => ({
     margin: '50px',
     paddingTop: theme.spacing.unit * 2,
     paddingBottom: theme.spacing.unit * 2,
+  },
+  flex: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  topicsWindow: {
+    width: '30%',
+    height: '300px',
+    borderRight: '2px solid grey'
+  },
+  chatWindow: {
+    width: '70%',
+    height: '300px',
+    padding: '20px'
+  },
+  chatBox: {
+    width: '85%'
+  },
+  button: {
+    width: '15%'
   },
 });
 
@@ -26,6 +52,37 @@ function Dashboard(props) {
         <Typography variant="h5" component="h5">
           Topic Placeholder
         </Typography>
+        <div className={classes.flex}>
+          <div className={classes.topicsWindow}>
+            <List>
+              {
+                ['topic'].map(topic => (
+                  <ListItem key={topic} button>
+                    <ListItemText primary={topic} />
+                  </ListItem>
+                ))
+              }
+            </List>
+          </div>
+          <div className={classes.chatWindow}>
+            {
+              [{from: 'user', msg: 'hello'}].map((chat, i) => (
+                <div className={classes.flex} key={i}>
+                  <Chip label={chat.from} avatar={<Avatar src="/static/images/avatar/1.jpg" />} />
+                  <Typography variant='p'>{chat.msg}</Typography>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+        <div className={classes.flex}>
+          <div className={classes.chatBox}>
+
+          </div>
+          <div className={classes.button}>
+
+          </div>
+        </div>
       </Paper>
     </div>
     </div>
