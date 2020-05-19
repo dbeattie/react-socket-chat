662cb0ef4a5ee94b6d8554dfbf5f4ed5a7a93415
Darren Beattie <darren@fettle.ca>
2020-05-19 14:34:04 -0700

    add sockets.io server so user can connect to client

--- src/Store.js
+++ src/Store.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import io from 'socket.io-client';
 
 export const CTX = React.createContext();
 
@@ -30,8 +31,13 @@ function reducer(state, action) {
       return state;
   }
 }
+let socket;
 
 export default function Store(props) {
+
+  if (!socket) {
+    socket = io(':3001');
+  }
   
   const reducerHook = React.useReducer(reducer, initState);
 
