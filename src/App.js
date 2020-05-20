a9b7619734546490e03346b434da9c1d8043422f
Darren Beattie <darren@fettle.ca>
2020-05-20 16:09:46 -0700

    made note in app.js about returning to this project

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Dashboard from './Dashboard';
 import Store from './Store';
 import './App.css';
 
+//I don't seem to be getting anything stored in state (local storage) as the result of using context or the reducer see https://www.youtube.com/watch?v=hiiaHyhhwBU&t=3017s but there is no repo so hard to know where I went wrong at the moment
+
 
 function App() {
   return (
